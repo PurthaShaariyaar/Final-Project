096eabdebfff972480863fb65d5c4bd876f94392
PurthaShaariyaar <92152517+PurthaShaariyaar@users.noreply.github.com>
2021-12-05 23:06:16 -0500

    Update index.php

--- homePage/index.php
+++ homePage/index.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="style/styles.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <title>OTU Cinema</title>
-    <link rel="icon" type="image/png" href="img/logo.png">
+    <link rel="icon" type="image/jpg" href="homepage/logo.jpg">
     <script src="_.js "></script>
 </head>
 
