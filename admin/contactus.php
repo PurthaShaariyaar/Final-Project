10d697532c2d454c46e0ffea459314023cbd3ca8
Antonio Lio 100805668 <95325793+AlfaQHard@users.noreply.github.com>
2021-12-06 19:19:20 -0500

    Update contactus.php

--- admin/contactus.php
+++ admin/contactus.php
@@ -1,7 +1,6 @@
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <title> Customer Messages</title>
     <meta charset="UTF-8">
@@ -9,20 +8,67 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     
     <link rel="icon" type="image/png" href="../img/logo.png">
-    <link rel="stylesheet" href="../style/styles.css">
+    <link rel="stylesheet" href="../styles.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 
+<style>
+    #BannerSpace {
+    height: 60px;
+    opacity: 0%;
+    }
+</style>
+
 <body>
     
+    <?php
+
+    include "config.php";
+    $host = "localhost"; /* Host name */
+    $user = "root"; /* User */
+    $password = ""; /* Password */
+    $dbname = "cinema_db"; /* Database name */
+
+    $select = "SELECT * FROM `feedbacktable`";
+    $run = mysqli_query($con, $select);
+
+    $sql = "SELECT * FROM bookingtable";
+    $bookingsNo = mysqli_num_rows(mysqli_query($con, $sql));
+    $messagesNo = mysqli_num_rows(mysqli_query($con, "SELECT * FROM feedbacktable"));
+    ?>
+
     <?php include('header.php'); ?>
     
     <div class="admin-container">
-        
+
         <?php include('sidebar.php'); ?>
-        <div class="container-lg">
-            <div class="table-responsive">
+
+        <div class="admin-section-column">
+
+            <div id=BannerSpace></div>
+            <div class="col-lg-8">
+                <div class="admin-section-stats">
+                        <div class="admin-section-stats-panel" style="border: none">
+                        </div>
+                        <div class="admin-section-stats-panel" style="border: none">
+                        </div>
+                        <div class="admin-section-stats-panel" style="border: none">
+                        </div>
+                        <div class="admin-section-stats-panel" style="border: none">
+                        </div>
+                        <div class="admin-section-stats-panel" style="border: none">
+                            <i class="fas fa-envelope" style="background-color: #3cbb6c"></i>
+                            <h2 style="color: #3cbb6c"><?php echo $messagesNo ?></h2>
+                            <h3>Messages</h3>
+                        </div>
+                        <div class="admin-section-stats-panel" style="border: none" width="100%">
+                        </div>
+                    </div>
+                <div id=BannerSpace></div>
+                </div>
+
+                <div class="admin-section-panel">
                 <div class="table-wrapper">
                     <div class="table-title">
                         <div class="row">
@@ -41,33 +87,26 @@
                             <th>Feedback</th>
                             <th>More</th>
                         </tr>
+
                         <tbody>
                             <?php
-                            include "config.php";
-                            $host = "localhost"; /* Host name */
-                            $user = "root"; /* User */
-                            $password = ""; /* Password */
-                            $dbname = "cinema_db"; /* Database name */
-
-                            $select = "SELECT * FROM `feedbacktable`";
-                            $run = mysqli_query($con, $select);
                             while ($row = mysqli_fetch_array($run)) {
                                 $id = $row['msgID'];
                                 $firstname = $row['senderfName'];
                                 $lastname = $row['senderlName'];
                                 $email = $row['sendereMail'];
                                 $message = $row['senderfeedback'];
-
                             ?>
+                            
                                 <tr align="center">
-                                    <td><?php echo $id; ?></td>
-                                    <td><?php echo $firstname; ?></td>
-                                    <td><?php echo $lastname; ?></td>
-                                    <td><?php echo $email; ?></td>
-                                    <td><?php echo $message; ?></td>
-                                    <!--<td><?php echo  "<a href='Deletecontact.php?id=" . $row['msgID'] . "'>delete</a>"; ?></td>-->
-                                    <td><button value="Book Now!" type="submit" onclick="" type="button" class="btn btn-danger"><?php echo  "<a href='Deletecontact.php?id=" . $row['msgID'] . "'>delete</a>"; ?></button></td>
-                                </tr>
+                                        <td><?php echo $id; ?></td>
+                                        <td><?php echo $firstname; ?></td>
+                                        <td><?php echo $lastname; ?></td>
+                                        <td><?php echo $email; ?></td>
+                                        <td><?php echo $message; ?></td>
+                                        <!--<td><?php echo  "<a href='Deletecontact.php?id=" . $row['msgID'] . "'>delete</a>"; ?></td>-->
+                                        <td><button value="Book Now!" type="submit" onclick="" type="button" class="btn btn-outline-danger"><?php echo  "<a href='Deletecontact.php?id=" . $row['msgID'] . "'>Delete</a>"; ?></button></td>
+                                    </tr>
 
                             <?php }
                             ?>
